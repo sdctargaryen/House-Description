322a27cac0a5511ce7931e556351a93a9dbd5da5
Anthony Kim <tony@Anthonys-MacBook-Pro.local>
2019-04-10 16:12:49 -0700

    completed first part of share modal

--- client/src/components/Share.jsx
+++ client/src/components/Share.jsx
@@ -1,33 +1,62 @@
 import React from 'react';
 import { Modal } from "react-bootstrap";
-import sharestyle from './savestyle.css';
+import sharestyle from './sharestyle.css';
 
 class Share extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      
+      link: false,
+      showEmbed: false
     }
+    this.copyLink = this.copyLink.bind(this)
   }
+
+  copyLink(){
+    this.setState({
+      link: true
+    })
+    setInterval(() => {this.setState({
+      link: false
+    })}, 2500)
+  }
+
   render(){
     return(
       <Modal {...this.props}>
         <Modal.Body>
           <div className={sharestyle.exit}><img src='https://s3-us-west-1.amazonaws.com/sharebnbicons/close+icon.png' onClick={this.props.onHide}></img></div>
           <h3>Share</h3>
-          <div>Check out this awesome listing on Airbnb: </div>
-          <img src="https://s3-us-west-1.amazonaws.com/sharebnbicons/facebook+icon.png" />
-          <div>Facebook</div>
-          <img src="https://s3-us-west-1.amazonaws.com/sharebnbicons/twitter+icon.png" />
-          <div>Twitter</div>
-          <img src="https://s3-us-west-1.amazonaws.com/sharebnbicons/email+icon.png" />
-          <div>Email</div>
-          <img src="https://s3-us-west-1.amazonaws.com/sharebnbicons/messenger+icon.png" />
-          <div>Messenger</div>
-          <img src="https://s3-us-west-1.amazonaws.com/sharebnbicons/copy+icon.png" />
-          <div>Link Copied / Copy Link</div>
-          <img src="https://s3-us-west-1.amazonaws.com/sharebnbicons/new+embed.png" />
-          <div>Embed</div>
+          <div className={sharestyle.shareDesc}>
+            Check out this awesome listing on Airbnb: {this.props.property} - 
+            Property for rent in {this.props.location}
+          </div>
+          <div>
+            <div className={sharestyle.shareMethod}>
+              <div className={sharestyle.iconDiv}><img src="https://s3-us-west-1.amazonaws.com/sharebnbicons/facebook+icon.png" className={sharestyle.shareFBIcon} /></div>
+              <div className={sharestyle.shareLink}>Facebook</div>
+            </div>
+            <div className={sharestyle.shareMethod}>
+              <div className={sharestyle.iconDiv}><img src="https://s3-us-west-1.amazonaws.com/sharebnbicons/twitter+icon.png" className={sharestyle.shareTIcon} /></div>
+              <div className={sharestyle.shareLink}>Twitter</div>
+            </div>
+            <div className={sharestyle.shareMethod}>
+              <div className={sharestyle.iconDiv}><img src="https://s3-us-west-1.amazonaws.com/sharebnbicons/email+icon.png" className={sharestyle.shareIcon} /></div>
+              <div className={sharestyle.shareLink}>Email</div>
+            </div>
+            <div className={sharestyle.shareMethod}>
+              <div className={sharestyle.iconDiv}><img src="https://s3-us-west-1.amazonaws.com/sharebnbicons/messenger+icon.png" className={sharestyle.shareIcon} /></div>
+              <div className={sharestyle.shareLink}>Messenger</div>
+            </div>
+            <div className={sharestyle.shareMethod}>
+              <div className={sharestyle.iconDiv}><img src="https://s3-us-west-1.amazonaws.com/sharebnbicons/copy+icon.png" className={sharestyle.shareIcon} /></div>
+              <div className={sharestyle.shareLink} onClick={this.copyLink}>{this.state.link ? 'Link Copied' : 'Copy Link'}</div>
+            </div>
+            <div className={sharestyle.shareMethod}>
+              <div className={sharestyle.iconDiv}><img src="https://s3-us-west-1.amazonaws.com/sharebnbicons/new+embed.png" className={sharestyle.shareIcon} /></div>
+              <div className={sharestyle.shareLink}>Embed</div>
+            </div>
+          </div>
         </Modal.Body>
       </Modal>
     )
