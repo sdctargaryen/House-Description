96357787cfb4e88a481a87ae4cabf1e0e9aee2df
Anthony Kim <tony@Anthonys-MacBook-Pro.local>
2019-04-12 19:35:46 -0700

    modifed css

--- client/src/components/HostOverview.jsx
+++ client/src/components/HostOverview.jsx
@@ -13,13 +13,11 @@ const HostOverview = (props) => {
             {propertyInfo.location}
           </div>
         </div>
-        <div className='hostContainer'>
+        <div className={hostStyle.hostContainer}>
           <div className={hostStyle.hostImg}>
-          {/* host image */}
             <img className={hostStyle.hostImg} src={host.pictureUrl}></img>
-            {/* <img className={hostStyle.hostImg} src='https://a0.muscache.com/im/pictures/cc41b0d2-f443-4c45-9dba-7dd52b5742c6.jpg?aki_policy=profile_x_medium'></img> */}
           </div>
-          <div className='hostName'>
+          <div className={hostStyle.hostName}>
             {host.name}
           </div>
         </div>
