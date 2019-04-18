da1eb8082b51bdbdcea47166c2696a3ecd2b5916
Albert Saputra <a.albert.saputra@gmail.com>
2019-04-17 18:28:34 -0700

    [fix] changed mongoose deprecated methods

--- database/index.js
+++ database/index.js
@@ -3,7 +3,7 @@ const propertySchema = require('./propertySchema.js');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost/airbnbDesc')
+mongoose.connect('mongodb://localhost/airbnbDesc', { useNewUrlParser: true })
   .then(()=>{console.log('connected to mongoDB')});
 
 var Property = mongoose.model('Property', propertySchema);
--- database/propertySchema.js
+++ database/propertySchema.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
-let propertySchema = mongoose.Schema ({
-  id: { type: Number, index: true, unique: true },
+let propertySchema = new mongoose.Schema ({
+  id: { type: Number },
   propertyInfo: {propType: String, title: String, location: String, numGuests: Number},
   beds: {quantity: Number, bedType: Array, iconUrl: Array},
   amenities: {basic: Array, notIncluded: Array, iconUrl: Array},
